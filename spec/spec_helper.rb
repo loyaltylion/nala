b9cc3ab55194aa92a9a3d11269788b49a58c2d74
Andy Pike <andy@andypike.com>
2015-05-18 21:21:12 +0100

    Add and setup rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require File.expand_path("../../lib/nala", __FILE__)
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.disable_monkey_patching!
+  config.backtrace_exclusion_patterns << /gems/
+  config.order = "random"
+end
